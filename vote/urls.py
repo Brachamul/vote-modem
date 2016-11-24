720988236681d7d65cf9546d6512ea02d7aee573
Barnaby Brachamul <barnaby.brachamul@gmail.com>
2016-11-24 21:44:45 +0100

    changed urls

--- vote/urls.py
+++ vote/urls.py
@@ -4,8 +4,8 @@ from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
+	url('^', views.vote, name='vote' ),
 	url(r'^(?P<code>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$', views.vote, name='vote_with_code' ),
 	url(r'^results/', views.results, name='results' ),
 	url(r'^list-codes/', views.list_codes, name='list_codes' ),
-	url('^', views.vote, name='vote' ),
 ]
